ea2951714e26b59006d6a612802da8dc84c89a9d
namdongyeob <ehdduq1324@gmail.com>
2026-05-26 05:00:23 +0900

    Delete src/main/java/com/example/teamprofile/global/exception/MemberNotFoundException.java

--- src/main/java/com/example/teamprofile/global/exception/MemberNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.teamprofile.global.exception;
-
-public class MemberNotFoundException extends RuntimeException {
-
-	public MemberNotFoundException(Long id) {
-		super("팀원을 찾을 수 없습니다. id: " + id);
-	}
-}
