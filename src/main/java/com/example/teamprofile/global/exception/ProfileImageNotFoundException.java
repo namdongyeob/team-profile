7d5725df2a51d3aa06aea706417b47dd529ddbf4
namdongyeob <ehdduq1324@gmail.com>
2026-05-26 05:01:07 +0900

    Delete src/main/java/com/example/teamprofile/global/exception/ProfileImageNotFoundException.java

--- src/main/java/com/example/teamprofile/global/exception/ProfileImageNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.teamprofile.global.exception;
-
-public class ProfileImageNotFoundException extends RuntimeException {
-
-	public ProfileImageNotFoundException(Long id) {
-		super("프로필 이미지가 없습니다. id: " + id);
-	}
-}
